21ab9a2319f2fdee12986bd83f10a3383632e8e7
Fabrice Marsaud <fabrice.marsaud@vates.fr>
2026-02-07 12:18:32 +0100

    clean useless code

--- Collide.lua
+++ Collide.lua
@@ -2,18 +2,8 @@ require("math-ext")
 local _, Trait = require("POO")()
 local Coord, _, _, Vector = require("Couple")()
 
-local colStates = {
-  BLOCK_1DX = 'blockX',
-  BLOCK_1DY = 'blockY',
-  PUSH_1DX = 'pushX',
-  PUSH_1DY = 'pushY'
-}
-
 local IACollide = Trait:new({
   IACollide = true,
-  _initColState = function (self)
-    self._colState = {}
-  end,
   _isRight = function (self, o)
     return self.d.x >= (o.d.x + o.w)
   end,
@@ -71,33 +61,23 @@ local IACollide = Trait:new({
     self._byYs = {}
   end,
 
-  _addColState = function (self, state)
-    if (not self._colState) then
-      self:_initColState()
-    end
-    table.insert(self._colState, state)
-  end,
-
   _storeByX = function (self, o)
     if not self._byXs then
       self._byXs = {}
     end
     table.insert(self._byXs, o)
   end,
-
   _storeByY = function (self, o)
     if not self._byYs then
       self._byYs = {}
     end
     table.insert(self._byYs, o)
   end,
-
   _popByX = function (self)
     if self._byXs then
       return table.remove(self._byXs)
     end
   end,
-
   _popByY = function (self)
     if self._byYs then
       return table.remove(self._byYs)
@@ -119,7 +99,6 @@ local IACollide = Trait:new({
     if pushBy then
       pushBy:blockX(self)
     end
-    self:_addColState(colStates.BLOCK_1DX)
   end,
 
   blockY = function (self, ...)
@@ -137,7 +116,6 @@ local IACollide = Trait:new({
     if pushBy then
       pushBy:blockY(self)
     end
-    self:_addColState(colStates.BLOCK_1DY)
   end,
 
   pushX = function (self, ...)
@@ -153,7 +131,6 @@ local IACollide = Trait:new({
       y = self._d.y
     })
     self.d = self._d:round()
-    self:_addColState(colStates.PUSH_1DX)
   end,
 
   pushY = function (self, ...)
@@ -169,35 +146,12 @@ local IACollide = Trait:new({
       y = by.d.y + by.h * math.sign(by.vector.y),
     })
     self.d = self._d:round()
-    self:_addColState(colStates.PUSH_1DY)
   end
 })
 
 local ICollideBlock = Trait:new({
   priority = 100,
-  _resolve = function (self, o)
-    -- if (
-    --   o.vector.x > 0 and -- moving right
-    --   o.d.x + o.w < self.d.x + self.w / 2 -- from the left
-    -- )
-    --   or
-    -- (
-    --   o.vector.x < 0 and -- moving left
-    --   o.d.x > self.d.x + self.w / 2 -- from the right
-    -- ) then
-    --     o:blockX(self)
-    -- end
-    -- if (
-    --   o.vector.y > 0 and -- moving down
-    --   o.d.y + o.h < self.d.y + self.h / 2 -- from top
-    -- )
-    --   or
-    -- (
-    --   o.vector.y < 0 and -- moving up
-    --   o.d.y > self.d.y + self.h / 2 -- from under
-    -- ) then
-    --     o:blockY(self)
-    -- end
+  _resolve = function ()
     return false
   end,
 
